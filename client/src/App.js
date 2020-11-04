762da4a8edce1d1099b1eafc2a21f3bf02e4a4ee
Dago R <dagoramirezserna@gmail.com>
2020-11-04 17:10:32 -0500

    Se instala react-router-dom y se crea la barra de menú

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
+import MenuBar from './components/MenuBar.js'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar/>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/register" component={Register}></Route>
+      </Container>      
+    </Router>
   );
 }
 
