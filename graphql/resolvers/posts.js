304ac9869a5400c041a96e43bb4afa42ee705022
Dago R <dagoramirezserna@gmail.com>
2020-11-03 22:45:50 -0500

    Se crea la aplicación de react y se configura apollo server

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -28,6 +28,10 @@ module.exports = {
     Mutation: {
         async createPost(_, { body }, context) {
             const user = checkAuth(context);
+
+            if(args.body.trim() === '') {
+                throw new Error('Post body must not be empty');
+            }
             
             const newPost = Post({
                 body,
