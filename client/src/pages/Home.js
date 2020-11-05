72bd1be26ad81773b8f39a7ebc6363a369c28101
Dago R <dagoramirezserna@gmail.com>
2020-11-04 21:01:05 -0500

    Se modifica el archivo Home y se crea el PostCard

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Grid } from 'semantic-ui-react';
+
+import PostCard from '../components/PostCard.js';
 
 export default function Home() {
+    const { loading, data = {} } = useQuery(FETCH_POST_QUERY);
+    const posts = data.getPosts;
     return (
-        <div>
-            <h1>Home page</h1>
-        </div>
+        <Grid columns={3} >
+            <Grid.Row className="page-title" >
+                <h1>Recent Post</h1>
+            </Grid.Row>
+            <Grid.Row>
+                {loading ? (
+                    <h1>Loading posts...</h1>
+                ) : (
+                    posts && posts.map(post => (
+                        <Grid.Column key={post.id} style={{marginBottom: 20 }} >
+                            <PostCard post={post} />
+                        </Grid.Column>
+                    ))
+                )}
+            </Grid.Row>
+        </Grid>
     )
 }
+
+const FETCH_POST_QUERY = gql`
+    {
+        getPosts {
+            id
+            body
+            createdAt
+            username
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments {
+                id
+                body
+                createdAt
+                username
+            }
+        }
+    }
+`;
