890b6b6721bb450c8247764d0d55466bc3f066b9
Dago R <dagoramirezserna@gmail.com>
2020-11-05 21:18:07 -0500

    Se realiza la validación del formulario de registro

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,7 +3,8 @@ import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 import { Button, Form } from 'semantic-ui-react';
 
-export default function Register() {
+export default function Register(props) {
+    const [errors, setErros] = useState({});
     const [values, setValues] = useState({
         username: '',
         email: '',
@@ -16,8 +17,13 @@ export default function Register() {
     }
 
     const [addUser, { loading }] = useMutation(REGISTER_USER, {
-        update(proxy, result) {
+        update(_, result) {
             console.log(result);
+            props.history.push('/');
+        },
+        onError(err) {
+            console.log(err.graphQLErrors[0].extensions.exception.errors)
+            setErros(err.graphQLErrors[0].extensions.exception.errors);
         },
         variables: values
     })
@@ -29,7 +35,7 @@ export default function Register() {
 
     return (
         <div className='form-container'>
-            <Form onSubmit={onSubmit} noValidate >
+            <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''} >
                 <h1>Register</h1>
                 <Form.Input
                     label='Username'
@@ -37,6 +43,7 @@ export default function Register() {
                     name='username'
                     type='text'
                     value={values.username}
+                    error={errors.username ? true : false}
                     onChange={onChange}
                 />
                 <Form.Input
@@ -45,6 +52,7 @@ export default function Register() {
                     name='email'
                     type='email'
                     value={values.email}
+                    error={errors.email ? true : false}
                     onChange={onChange}
                 />
                 <Form.Input
@@ -53,6 +61,7 @@ export default function Register() {
                     name='password'
                     type='password'
                     value={values.password}
+                    error={errors.password ? true : false}
                     onChange={onChange}
                 />
                 <Form.Input
@@ -61,12 +70,22 @@ export default function Register() {
                     name='confirmPassword'
                     type='password'
                     value={values.confirmPassword}
+                    error={errors.confirmPassword ? true : false}
                     onChange={onChange}
                 />
                 <Button type='submit' primary >
                     Register
                 </Button>
             </Form>
+            {Object.keys(errors).length > 0 && (
+                <div className='ui error message' >
+                    <ul className='list' >
+                        {Object.values(errors).map(value => (
+                            <li key={value} >{value}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
